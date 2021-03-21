e070072419e3e72ac65d08bb53725face74093f1
Rahul Patni <rahul20patni@gmail.com>
2021-03-20 21:40:08 -0700

    Adding axios and making the api call

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,27 +2,43 @@ import React from "react";
 import { View, TextInput, StyleSheet } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import babelConfig from "../../babel.config";
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
     <View style={styles.background}>
-      <Feather name="search" size={30} />
-      <TextInput placeholder="search" style={styles.inputStyle} />
+      <Feather name="search" size={30} style={styles.iconStyle} />
+      <TextInput
+        placeholder="Search"
+        style={styles.inputStyle}
+        value={term}
+        autoCorrect={false}
+        autoCapitalize="none"
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   background: {
-    backgroundColor: "#FOEEEE",
+    marginTop: 10,
+    backgroundColor: "#f0eeee",
     height: 50,
     borderRadius: 5,
     marginHorizontal: 15,
     flexDirection: "row",
+    alignItems: "center",
   },
   inputStyle: {
     borderColor: "black",
     borderWidth: 1,
     flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
   },
 });
 
--- src/screens/SearchScreens.js
+++ src/screens/SearchScreens.js
@@ -1,12 +1,39 @@
 import SearchBar from "../components/SearchBar";
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term,
+          location: "Incline Village",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (error) {
+      console.log(error);
+      setErrorMessage("something went wrong: " + error);
+    }
+  };
   return (
     <View>
-      <SearchBar></SearchBar>
-      <Text>Search Screen</Text>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={searchApi}
+      ></SearchBar>
+      {errorMessage ? (
+        <Text>{errorMessage}</Text>
+      ) : (
+        <Text>We have found {results.length} results</Text>
+      )}
     </View>
   );
 };
